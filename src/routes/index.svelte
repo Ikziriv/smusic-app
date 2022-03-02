3a388cf4428544ce6d020c06b8c425e41542d179
Rizky Ivan Darmawan <ivanriski007@gmail.com>
2022-03-03 00:26:53 +0700

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
     <div class="flex flex-col w-full h-72 bg-neutral-700">
       <div class="flex flex-row justify-between px-4">
         <div class="px-4 py-6">
-          <span class="font-bold text-xl text-white">Popular ( New Release )</span>
+          <span class="font-bold text-2xl text-white">Popular ( New Release )</span>
         </div>
         <div class="px-4 py-6 mt-1">
           <span class="font-normal text-sm text-white uppercase">See All</span>
@@ -14,7 +14,7 @@
     <div class="flex flex-col w-full h-72 bg-neutral-700">
       <div class="flex flex-row justify-between px-4">
         <div class="px-4 py-6">
-          <span class="font-bold text-xl text-white">Top Charts</span>
+          <span class="font-bold text-2xl text-white">Top Charts</span>
         </div>
         <div class="px-4 py-6 mt-1">
           <span class="font-normal text-sm text-white uppercase">See All</span>
@@ -25,7 +25,7 @@
     <div class="flex flex-col w-full h-72 bg-neutral-700">
       <div class="flex flex-row justify-between px-4">
         <div class="px-4 py-6">
-          <span class="font-bold text-xl text-white">Ragam Musik Indonesia</span>
+          <span class="font-bold text-2xl text-white">Ragam Musik Indonesia</span>
         </div>
         <div class="px-4 py-6 mt-1">
           <span class="font-normal text-sm text-white uppercase">See All</span>
