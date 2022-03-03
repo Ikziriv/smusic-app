7d375d1d24f43ddc3e4b70a2c8ab28625526457a
Rizky Ivan Darmawan <ivanriski007@gmail.com>
2022-03-03 23:41:13 +0700

    Update package

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,23 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({
+		postcss: {
+			plugins: [
+				cssnano({ 
+					preset: 'cssnano-preset-default' 
+				}), 
+				autoprefixer({})
+			]
+		},
+	}),
 
 	kit: {
 		adapter: adapter()
